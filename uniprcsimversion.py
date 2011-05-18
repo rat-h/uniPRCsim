9f4d3c782d4671d50f2ddf9b04b97f30e1bae00e
Ruben A. Tikidji-Hamburyan <rth@linux-fyxy.site>
2011-05-18 10:05:03 +0400

    create update system

--- uniprcsimversion.py
+++ uniprcsimversion.py
@@ -1,10 +1,36 @@
 
 MAJOR=0
 MINOR=0
-PATCH=01
-DATA="20110518"
+PATCH=2
+DATE="20110518"
 def getversion():
 	return "%d.%02d.%04d"%(MAJOR,MINOR,PATCH)
 def getlongversion():
 	#TODO: add here some information
-	return "uniPRCsim version %d.%02d.%04d (%s)"%(MAJOR,MINOR,PATCH,DATA)
+	return "uniPRCsim version %d.%02d.%04d (%s)"%(MAJOR,MINOR,PATCH,DATE)
+
+
+if __name__ == "__main__":
+	import time, re, os, sys
+	PATCH += 1
+	DATE = time.strftime("%Y%m%d",time.localtime())
+	#update *.py file
+	fdr = open ("uniprcsimversion.py", "r")
+	fdw = open ("nuniprcsimversion.py", "w")
+	for line in fdr.readlines():
+		fdw.write( re.sub("^DATE=\"\\d*\"","DATE=\"%s\""%DATE, re.sub("^PATCH=\\d*", "PATCH=%d"%PATCH,line ) ) )
+	
+	fdr.close()
+	fdw.close()
+	os.rename("nuniprcsimversion.py","uniprcsimversion.py")
+	import uniprcsimversion
+	if 	os.system("zip -9 uniPRCsim-update%s.zip *.pyc"%getversion()):
+		sys.stderr.write("Cannot create zip file\n\n")
+		sys.exit(1)
+	if os.system("git commit -a"):
+		sys.stderr.write("Cannot commit current version\n\n")
+		sys.exit(1)
+	if os.system("git tag \\\"v%s\\\""%getversion()):
+		sys.stderr.write("Cannot tag current version\n\n")
+		sys.exit(1)
+	
