adec93cd1eacd5e057dec302f5f97740a85f8bfa
Ruben A. Tikidji-Hamburyan <rth@linux-fyxy.site>
2011-07-27 11:30:01 -0500

    fix date of release

--- uniprcsimversion.py
+++ uniprcsimversion.py
@@ -2,7 +2,7 @@
 MAJOR=0
 MINOR=0
 PATCH=21
-DATE="20110618"
+DATE="20110727"
 def getversion():
 	return "%d.%02d.%04d"%(MAJOR,MINOR,PATCH)
 def getlongversion():
@@ -12,7 +12,16 @@ def getlongversion():
 
 if __name__ == "__main__":
 	import time, re, os, sys
+	DATE = time.strftime("%Y%m%d",time.localtime())
+	fdr = open ("uniprcsimversion.py", "r")
+	fdw = open ("nuniprcsimversion.py", "w")
+	for line in fdr.readlines():
+		fdw.write( re.sub("^DATE=\"\\d*\"","DATE=\"%s\""%DATE, line ) ) 
+	fdr.close()
+	fdw.close()
+	os.rename("nuniprcsimversion.py","uniprcsimversion.py")
 	import uniprcsimversion
+
 	if 	os.system("zip -9 uniPRCsim-update%s.z *.pyc *.py"%getversion()):
 		sys.stderr.write("Cannot create zip file\n\n")
 		sys.exit(1)
@@ -23,12 +32,10 @@ if __name__ == "__main__":
 		sys.stderr.write("Cannot tag current version\n\n")
 		sys.exit(1)
 	PATCH += 1
-	DATE = time.strftime("%Y%m%d",time.localtime())
 	fdr = open ("uniprcsimversion.py", "r")
 	fdw = open ("nuniprcsimversion.py", "w")
 	for line in fdr.readlines():
-		fdw.write( re.sub("^DATE=\"\\d*\"","DATE=\"%s\""%DATE, re.sub("^PATCH=\\d*", "PATCH=%d"%PATCH,line ) ) )
-	
+		fdw.write( re.sub("^PATCH=\\d*", "PATCH=%d"%PATCH,line ) )
 	fdr.close()
 	fdw.close()
 	os.rename("nuniprcsimversion.py","uniprcsimversion.py")
